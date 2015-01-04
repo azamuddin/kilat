7f2f3ef99e805537d966447b5989d2f04eb5ae41
Muhammad Azamuddin <azamuddin@live.com>
2015-01-04 19:34:37 +0700

    ADD: Shophisticated manual filter is finished

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -3,6 +3,7 @@
 class AdminController extends \BaseController {
 
 	protected $data_view;
+	protected $filter = array();
 
 	public function __construct()
 	{
@@ -12,7 +13,26 @@ class AdminController extends \BaseController {
 
 	protected function filterData($model)
 	{
-		
+
+		// Tangkap filter
+		if(Input::get('search'))
+		{
+			$this->filter = Input::except('search');
+		}
+
+		$result = $model::orWhere(function($query)
+		{
+			foreach($this->filter as $criteria => $value)
+			{
+				if($value)
+				{
+					$query->where($criteria,"like", "%$value%");
+				}
+				
+			}
+		});
+
+		return $result;
 	}
 	
 
--- app/controllers/admin/ManageUsersController.php
+++ app/controllers/admin/ManageUsersController.php
@@ -2,32 +2,12 @@
 
 class ManageUsersController extends AdminController {
 
-	public $filter = array('username'=>'', 'email'=>'');
-
 	public function index()
 	{
 		$data_view = $this->data_view;
 
-		// Catch filter
-		if(Input::get('search'))
-		{
-			$this->filter = Input::except('search');
-		}
-
 		// User and filtering
-		$users = User::orWhere(function($query)
-		{
-			foreach($this->filter as $criteria => $value)
-			{
-				if($value)
-				{
-					$query->where($criteria,"like", "%$value%");
-				}
-				
-			}
-		})->paginate(4);
-
-		$users = $this->filterData(new User)->paginate(4);
+		$users = $this->filterData(new User)->paginate(10);
 
 		$data_view['model'] = $users;
 
