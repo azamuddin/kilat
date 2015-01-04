a1f8014484010505b4268ad1025d2625c1657484
Muhammad Azamuddin <azamuddin@live.com>
2015-01-04 10:03:37 +0700

    FIRST INITIALIZATION

---
+++ app/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+//
+
+// Confide RESTful route
+Route::get('users/confirm/{code}', 'UsersController@getConfirm');
+Route::get('users/reset_password/{token}', 'UsersController@getReset');
+Route::get('users/reset_password', 'UsersController@postReset');
+Route::controller( 'users', 'UsersController');
